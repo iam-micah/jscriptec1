030940fbcf69f3daa8ac7db933f9f0af40a2dfa8
iam-micah <79053404+iam-micah@users.noreply.github.com>
2022-10-17 19:24:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function main_function() {
 
-    if (document.getElementById("button").innerHTML == "JUMP >") {
+    if (document.getElementById("button").innerHTML == "JUM") {
         console.log("first condition");
-        document.getElementById("button").innerHTML = "< JUMP";
+        document.getElementById("button").innerHTML = "JUMP";
         var text = document.getElementById("left-box").value;
         document.getElementById("right-box").value = text;
         document.getElementById("left-box").value = "";
         
-    } else if (document.getElementById("button").innerHTML == "< JUMP") {
+    } else if (document.getElementById("button").innerHTML == "JUMP") {
         console.log("second condition");
-        document.getElementById("button").innerHTML = "JUMP >"
+        document.getElementById("button").innerHTML = "JUM"
         var text = document.getElementById("right-box").value;
         document.getElementById("left-box").value = text;
         document.getElementById("right-box").value = "";
