941f48c260c40500c001ef6458edd7ba3230a114
iam-micah <79053404+iam-micah@users.noreply.github.com>
2022-10-17 18:14:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function main_function() {
 
-    if (document.getElementById("button").innerHTML == "wow") {
+    if (document.getElementById("button").innerHTML == "JUMP >") {
         console.log("first condition");
-        document.getElementById("button").innerHTML = "owo";
+        document.getElementById("button").innerHTML = "< JUMP";
         var text = document.getElementById("left-box").value;
         document.getElementById("right-box").value = text;
         document.getElementById("left-box").value = "";
         
-    } else if (document.getElementById("button").innerHTML == "owo") {
+    } else if (document.getElementById("button").innerHTML == "< JUMP") {
         console.log("second condition");
-        document.getElementById("button").innerHTML = "wow"
+        document.getElementById("button").innerHTML = "JUMP >"
         var text = document.getElementById("right-box").value;
         document.getElementById("left-box").value = text;
         document.getElementById("right-box").value = "";
